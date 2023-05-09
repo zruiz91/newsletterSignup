630fabdedfd9fa2978c041e719df96ecf24dc80d
Zach Ruiz <powersbooth10@gmail.com>
2023-05-09 18:00:34 -0500

    set up the full functionality of site. excluded api key and unique id from the commit.

--- app.js
+++ app.js
@@ -11,25 +11,68 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('listening on 30000.');
 
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/signup.html');
 });
 
-app.post('/', function(req, res) {
+app.post('/', function (req, res) {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
 
-    console.log(firstName + ' ' + lastName + ' ' + email)
+
+
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/x";
+
+    const options = {
+        method: 'POST',
+        auth: "zruiz91:x"
+    }
+
+    const request = https.request(url, options, function (response) {
+
+        if (response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
 
 });
+
+app.post('/failure', function(req, res) {
+    res.sendFile(__dirname + "/signup.html")
+});
+
