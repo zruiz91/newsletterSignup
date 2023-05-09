c20fa7f032e301aac279341b00bde79d3edc7a71
Zach Ruiz <powersbooth10@gmail.com>
2023-05-09 16:02:56 -0500

    set up the post request.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//code for setting up the server
 const express = require('express');
 
 const bodyParser = require('body-parser');
@@ -11,6 +11,11 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
 app.listen(3000, function() {
     console.log('listening on 30000.');
 
@@ -18,4 +23,13 @@ app.listen(3000, function() {
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', function(req, res) {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    console.log(firstName + ' ' + lastName + ' ' + email)
+
 });
